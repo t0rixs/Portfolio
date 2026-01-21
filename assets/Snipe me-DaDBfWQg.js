9c1a00924daa1bb9b5fa54dede7d861c6b7f3e28
t0rixs <shutamix@gmail.com>
2026-01-21 11:15:04 +0900

    Updates

---
+++ assets/Snipe me-DaDBfWQg.js
@@ -0,0 +1,14 @@
+const n=`### 制作期間
+2025年10月8-9日
+### 開発環境
+
+[![Languages](https://skillicons.dev/icons?i=react,js)](https://skillicons.dev)
+
+### 1. ゲームの概要
+
+指定回数の弾丸で、すべての立体を貫くゲームです。
+three.jsを使用しています。
+思いつきで作りました。
+
+
+`;export{n as default};
